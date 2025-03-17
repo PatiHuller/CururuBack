66d2c52f7a7c650c4f58287fd0f4e5eb16b891e6
patricia_araldi@hotmail.com <patricia_araldi@hotmail.com>
2025-03-17 16:09:59 -0300

    first commit

--- routes/productos.js
+++ routes/productos.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const api = express.Router();
 const upload = require('../libs/storage'); 
-const authenticateToken = require('../middlewares/authenticateToken.js');
+const authenticateToken = require('../middlewares/authenticateToken');
 
 
 const { 
